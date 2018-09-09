18f8328b193d9d46232c7c95af589c18349bd1ab
Jesse Riggins <jesse.riggins98@gmail.com>
2018-09-09 05:04:37 -0400

    Made roof invisible. Boxes do not collide with the roof. Moved walls out so they are not on the play field.

---
+++ Crushed/Assets/IgnoreRoof.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreRoof : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Physics2D.IgnoreCollision(this.gameObject.GetComponent<BoxCollider2D>(), GameObject.FindGameObjectWithTag("Roof").GetComponent<BoxCollider2D>());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
