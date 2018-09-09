660014f961c812290b2c49817ddc81bf858c34bc
Jesse Riggins <jesse.riggins98@gmail.com>
2018-09-09 04:37:54 -0400

    Adds trail to boxes and enemies.

---
+++ Crushed/Assets/DeleteParticles.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteParticles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.GetComponent<Rigidbody2D>().rotation != 0)
+        {
+            Destroy(this.GetComponent<ParticleSystem>());
+        }
+	}
+}
